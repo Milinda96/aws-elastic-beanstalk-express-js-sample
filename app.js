6d868d8d9a9fa7dc3e160851043ad54e72fddb77
Milinda Senaka <milindasenaka.1@gmail.com>
2021-01-01 16:04:41 +0530

    testing pipeline

--- app.js
+++ app.js
@@ -4,5 +4,13 @@ const port = 8080;
 
 app.get('/', (req, res) => res.send('Testing CI/CD pipleline'));
 
+app.post('/example', (req, res) => {
+    res.send('new student posting');
+});
+
+app.put('/example/:id', (req, res) => {
+    res.send('Updating existing record');
+});
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
